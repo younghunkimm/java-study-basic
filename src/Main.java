d50d17f02db5d1f6b9782e2919e820145bd14281
김영훈 <91935220+younghunkimm@users.noreply.github.com>
2025-07-07 21:00:34 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import chapter1.MyNewClass;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        new MyNewClass();
+    }
+}
